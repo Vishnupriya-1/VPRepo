21d33c13b30a52f2599782c59d8bbec1648a4852
Vishnupriya-1 <89778255+Vishnupriya-1@users.noreply.github.com>
2021-09-07 23:55:16 +0530

    Update StockApplicationTests.java

--- stock/src/test/java/com/inventory/stock/StockApplicationTests.java
+++ stock/src/test/java/com/inventory/stock/StockApplicationTests.java
@@ -1,13 +1,63 @@
 package com.inventory.stock;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.inventory.stock.model.Stock;
+import com.inventory.stock.repository.StockRepository;
+import com.inventory.stock.service.StockService;
 
 @SpringBootTest
 class StockApplicationTests {
+	
+	@Autowired
+	private StockService stockService;
+	
+	@Autowired
+	private StockRepository stockRepo;
 
+	/*
+	 * Test method to create Stock
+	 */
 	@Test
-	void contextLoads() {
-	}
+	public void testCreateStock() {
+		Stock stock = new Stock(14L,"stock20",190,new Date(),90);
+		stockService.create(stock);
 
+		assertNotNull(stockService.findByStockName("stock20"));
+	}
+	
+	/*
+	 * Test method to get all Stock
+	 */
+	@Test
+	public void testGetAllStock()
+	{
+		List<Stock> stockList = stockRepo.findAll();
+		assertThat(stockList).size().isGreaterThan(0);
+	}
+	
+	/*
+	 * Test method to update Stock
+	 */
+	@Test
+	public void testUpdateStock()
+	{
+		Stock stock = stockService.findByStockName("stock9");
+		stock.setQuantity(4);
+		stockService.update(stock);
+		assertNotEquals(4, stockService.findByStockName("stock9").getQuantity());
+		assertEquals(10, stockService.findByStockName("stock9").getQuantity());
+	}
 }
+
